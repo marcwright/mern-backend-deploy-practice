c80a45c04e5b06ce98c8bdf8e8309f452ae3a895
Marc Wright <marc@generalassemb.ly>
2020-03-25 11:36:58 -0500

    Add process.env.NODE_ENV

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 // set the uri for connecting to our local mongodb
 let mongoURI = ''
 
-if (process.env.MONGODB_URI) {
+if (process.env.NODE_ENV === "production") {
   mongoURI = process.env.MONGODB_URI;
 } else {
   mongoURI = "mongodb://localhost/mern-lab";
